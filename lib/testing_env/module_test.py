80406bf32220b287b761f23c18bb013f0dfee954
charles <cyphersmae@gmail.com>
2025-08-28 15:18:27 +0300

    p3-p

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -7,12 +7,16 @@ from versions import (
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 8
+    assert version_info.minor in [8, 9, 10, 11, 12]
+
+
 
 
 def test_requests_version():
-    assert requests_version() == "2.27.1"
+
+    assert requests_version() in ["2.27.1", "2.31.0"]
+
 
 
 def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+    assert pytest_version() == "7.4.4"
